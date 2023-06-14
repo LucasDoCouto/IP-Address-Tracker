6ad9f59bf74d5afc072a78ff2cd51bff92d3c1e6
Lucas do Couto <48269935+LucasDoCouto@users.noreply.github.com>
2023-06-14 10:17:56 -0400

    Iniciando a integração com a API

---
+++ apikey-template.js
@@ -0,0 +1,3 @@
+// Rename this file to apikey.js
+// Replace the content to use your own API Key
+const apiKEY = "yourapikeyhere"
---
+++ index.js
@@ -0,0 +1,22 @@
+// Declaração de variáveis e constantes
+const url = "https://geo.ipify.org/api/v2/country?apiKey="
+var ip = "8.8.8.8"
+
+const dadosAPI = {
+    locationCountry: " ",
+    locationRegion: " ",
+    locationTimezone: " ",
+    locationISP: " "
+}
+
+alert(url + ip)
+
+// Get data
+
+async function getData(){
+    endereco = url + ip
+    const response = await fetch(endereco)
+    console.log(response)
+}
+
+getData()
