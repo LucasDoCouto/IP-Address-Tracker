cefb2fd89616ad57130229f2b728d9ae05bf4c2b
Lucas do Couto <48269935+LucasDoCouto@users.noreply.github.com>
2023-06-15 12:02:36 -0400

    Função de consumo de API

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
+
 // // Declaração de variáveis e constantes
-// const url = "https://geo.ipify.org/api/v2/country?apiKey="
-var ip = "8.8.8.8"
+var arrowButton = document.querySelector(".btn-arrow")
 
-// const dadosAPI = {
-//     locationCountry: " ",
-//     locationRegion: " ",
-//     locationTimezone: " ",
-//     locationISP: " "
-// }
+let ipAddress
 
-// alert(url + ip)
-
-// // Get data
-
-// async function getData(){
-//     endereco = url + ip
-//     const response = await fetch(endereco)
-//     console.log(response)
-// }
-
-// getData()
+// Replace and use your own API Key
+// Substitua e use sua própria chave de API
 
 
-// Map Leaflet
+// Função de integração com a API
+
+function getLocation() {
+    var apiKEY = "at_x5svTZkGTky0m9UU7C2d4VHGvIJPl"
+    var apiURL = "https://geo.ipify.org/api/v2/country?apiKey="
+    var ip = "8.8.8.8"
+    var url = apiURL + apiKEY + '&ipAddress=' + ip
+    // requisição com o Fetch
+    fetch(url)
+        .then(consulta => consulta.json())
+        .then(consulta => {
+            ipAddress = consulta.ip
+        })
+    
+    alert(ipAddress)
+
+}
+
+arrowButton.addEventListener("click",function(){
+    getLocation()
+})
+
+// Leaflet Map
 
 var map = L.map('map').setView([51.505, -0.09], 13);
 
@@ -31,5 +39,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup(ip)
+    .bindPopup(ipAddress)
     .openPopup();
